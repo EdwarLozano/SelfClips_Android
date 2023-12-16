bd029de7ee257dd1b654ef6424795328732a5dc5
edwar.lozano <edwar.lozano@mail.escuelaing.edu.co>
2023-12-15 19:14:47 -0500

    Login y Register Empezando

---
+++ app/src/main/java/com/example/moviefinderapp/ui/Activity/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.example.moviefinderapp.ui.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.moviefinderapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView btn = findViewById(R.id.textView2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
